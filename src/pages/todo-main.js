ab2f642f11a3a600ccf6bc393121a1e464525458
jyotikabanerjee <jyotikabanerjee@gmail.com>
2021-07-15 00:18:39 -0700

    add src code

--- src/pages/todo-main.js
+++ src/pages/todo-main.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './todo-main.css';
-// import PropTypes from 'prop-types';
+import TodoItem from "../components/item/todo-item";
 
 const TodoMain = () => {
+    let [todoItem, setTodoItem] = useState({});
+    const handleKeyDown = (ev) => {
+        // console.log(ev.key);
+        if(ev.key === 'Enter') {
+            console.log(ev.target.value);
+            let obj = {
+                'task': ev.target.value,
+                'done': false
+            };
+            setTodoItem(obj);
+        }
+    };
+
     return (
         <div>
             <h1 className="Todo-header">
                 todos
              </h1>
             <div className="Todo-container">
-                <input id="Todo-enter" type="text" placeholder="What needs to be done?"/>
+                <input id="Todo-enter" type="text" placeholder="What needs to be done?" onKeyDown={handleKeyDown}/>
             </div>
+            <TodoItem todo={todoItem}/>
         </div>
     );
 };
