802cc3ba9a5fd6a3fb782f382f47f895984c4e8f
jyotikabanerjee <jyotikabanerjee@gmail.com>
2021-07-14 23:06:18 -0700

    add src code

---
+++ src/pages/todo-main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './todo-main.css';
+// import PropTypes from 'prop-types';
+
+
+const TodoMain = () => {
+    return (
+        <div>
+            <h1 className="Todo-header">
+                todos
+             </h1>
+            <div className="Todo-container">
+                <input id="Todo-enter" type="text" placeholder="What needs to be done?"/>
+            </div>
+        </div>
+    );
+};
+
+export default TodoMain;
