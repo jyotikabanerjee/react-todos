8293edde30ae91e6bb7b871b2ead46e0a7b5803b
jyotikabanerjee <jyotikabanerjee@gmail.com>
2021-07-15 23:24:31 -0700

    routing 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
 import TodoMain from "./pages/todos/todo-main";
+import Home from "./pages/home/home"
+import Photos from "./pages/photos/photos"
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import * as classes from './app.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TodoMain/>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <TodoMain/>
+    //   </header>
+    // </div>
+      <Router>
+          <ul className="App-header">
+              <li>
+                  <Link to="/">Home</Link>
+              </li>
+              <li>
+                  <Link to="/react-todos"> Todos </Link>
+              </li>
+              <li>
+                  <Link to="/react-photos"> Photos </Link>
+              </li>
+          </ul>
+          <Switch>
+              <Route path="/react-todos">
+                  <TodoMain/>
+              </Route>
+              <Route path="/react-photos">
+                  <Photos/>
+              </Route>
+              <Route path="/">
+                  <Home/>
+              </Route>
+          </Switch>
+      </Router>
   );
 }
 
---
+++ src/pages/photos/photos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Photos = ({}) => {
+    return (
+        <div>
+            Build something related to photos here
+        </div>
+)
+    ;
+};
+
+Photos.propTypes = {};
+Photos.defaultProps = {};
+
+export default Photos;
